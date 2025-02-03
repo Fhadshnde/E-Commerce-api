8cc133b5a200938cfaa0e39acf9997de16fa69e7
Fhadshnde <Fhadshnde32@gmail.com>
2025-02-04 00:27:41 +0300

    com9

--- models/Product.js
+++ models/Product.js
@@ -4,7 +4,6 @@ const ProductSchema = new mongoose.Schema(
   {
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
-    img: { type: String, required: true },
     categories: { type: Array },
     price: { type: Number, required: true },
   },
